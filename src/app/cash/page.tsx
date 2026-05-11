d959bab2795abff7b77a6adb76da4d4013efa3bf
agencybooks <admin@agencybooks.io>
2026-05-11 07:55:56 -0500

    Enable dynamic rendering for CashPage
    Add dynamic export to CashPage component

--- src/app/cash/page.tsx
+++ src/app/cash/page.tsx
@@ -1,6 +1,8 @@
 import { getCashData, getOpenInvoices, getAlerts, getClients } from "@/lib/db/services"
 import { CashPageContent } from "@/components/cash-page-content"
 
+export const dynamic = "force-dynamic"
+
 export default async function CashPage() {
   const [cashData, openInvoices, alerts, clients] = await Promise.all([
     getCashData(),
