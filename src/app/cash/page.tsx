f9c2fe3d8a1895c6402d062b1809827344b1264e
agencybooks <admin@agencybooks.io>
2026-05-11 08:21:16 -0500

    Update page.tsx

--- src/app/cash/page.tsx
+++ src/app/cash/page.tsx
@@ -1,8 +1,5 @@
-import { Suspense } from "react"
 import { getCashData, getOpenInvoices, getAlerts, getClients } from "@/lib/db/services"
 import { CashPageContent } from "@/components/cash-page-content"
-
-export const dynamic = "force-dynamic"
 
 export default async function CashPage() {
   const [cashData, openInvoices, alerts, clients] = await Promise.all([
@@ -12,9 +9,5 @@ export default async function CashPage() {
     getClients(),
   ])
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <CashPageContent cashData={cashData} openInvoices={openInvoices} alerts={alerts} clients={clients} />
-    </Suspense>
-  )
+  return <CashPageContent cashData={cashData} openInvoices={openInvoices} alerts={alerts} clients={clients} />
 }
